3e351cc4227ed9d99f562e8e3d0b516451998c3e
Usman Aslam <uaslam@iconect.com>
2024-03-20 13:15:49 -0400

    adding ppt links

--- src/index.ts
+++ src/index.ts
@@ -8,10 +8,12 @@ WebViewer({
   .then(function (instance) {
     const { documentViewer } = instance.Core;
 
-    const HYLAND_FILE = "https://www.dropbox.com/scl/fi/sbdh3fvbua8wte0zd8yeh/hyland-linearized-file.pdf?rlkey=rv234ijlma1xnos6sncj2f3m8&dl=1"
+    const NO_LINEAR_FILE = "https://www.dropbox.com/scl/fi/sbdh3fvbua8wte0zd8yeh/hyland-linearized-file.pdf?rlkey=rv234ijlma1xnos6sncj2f3m8&dl=1"
     const APRYSE_FILE = "https://www.dropbox.com/scl/fi/ps2hc8d7n6rvlwb21ih7r/apryse-linearized-file.pdf?rlkey=vf13owdb3m3zf8lojgtyr5ok4&dl=1"
+    const HYLAND_PPT_PDF = "https://www.dropbox.com/scl/fi/lef1xqifv3jvbba0sz6ud/hyland-linearized-ppt.pdf?rlkey=a577zevgf77tq2896outmsn75&dl=1"
+    const APRYSE_PPT_PDF = "https://www.dropbox.com/scl/fi/3lbbgmryn2urk91tb01th/apyrse-linearized-ppt.pdf?rlkey=agusfys97xcobatf1h5d449q2&dl=1"
 
-    documentViewer.loadDocument(APRYSE_FILE, {
+    documentViewer.loadDocument(HYLAND_PPT_PDF, {
       extension: 'pdf',
     }).catch((error) => {
       console.error('Loading PDF Error', error);
