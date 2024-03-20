f8ec8ca5b0dde15c909e541498207f19895cc280
Usman Aslam <uaslam@iconect.com>
2024-03-20 12:56:26 -0400

    adding links

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
 /// <reference path="../webviewer.d.ts" />
 import WebViewer from '@pdftron/webviewer'
+
 WebViewer({
-    path: '/lib', // path to the PDFTron 'lib' folder on your server
-    licenseKey: 'Insert commercial license key here after purchase',
-    initialDoc: 'https://pdftron.s3.amazonaws.com/downloads/pl/webviewer-demo.pdf',
-  }, document.getElementById('viewer'))
-  .then(function(instance) {
-    const docViewer = instance.docViewer;
-    const annotManager = instance.annotManager;
-    // call methods from instance, docViewer and annotManager as needed
-  
-    // you can also access major namespaces from the instance as follows:
-    const Tools = instance.Tools;
-    const Annotations = instance.Annotations;
+  path: '/lib', // path to the PDFTron 'lib' folder on your server
+  licenseKey: 'demo:1706552234011:7f74471703000000001e9543f0bc50056637054255f7fec6003e61a9fc',
+}, document.getElementById('viewer'))
+  .then(function (instance) {
+    const { documentViewer } = instance.Core;
+
+    const HYLAND_FILE = "https://www.dropbox.com/scl/fi/sbdh3fvbua8wte0zd8yeh/hyland-linearized-file.pdf?rlkey=rv234ijlma1xnos6sncj2f3m8&dl=1"
+    const APRYSE_FILE = "https://www.dropbox.com/scl/fi/ps2hc8d7n6rvlwb21ih7r/apryse-linearized-file.pdf?rlkey=vf13owdb3m3zf8lojgtyr5ok4&dl=1"
+
+    documentViewer.loadDocument(APRYSE_FILE, {
+      extension: 'pdf',
+    }).catch((error) => {
+      console.error('Loading PDF Error', error);
+    });
   });
-
-
-
